dd29878c4d76acf7811a8d8a7bed73ab1c55dcb7
Vincent Liu <pkchu99@gmail.com>
2022-06-18 23:05:13 +1000

    implement lanczos

--- LanczosUpscaler/lanczos.c
+++ LanczosUpscaler/lanczos.c
@@ -6,15 +6,71 @@
 //
 
 #include "lanczos.h"
+#include <math.h>
+
+#define MAX(a, b) (a > b ? a : b)
+#define MIN(a, b) (a < b ? a : b)
+
+
+
+double sinc(double x) {
+    return sin(x)/x;
+}
+
+double lanczos_kernel(double x) {
+    return sinc(M_PI * x) * sinc(M_PI * x / LANCZOS_A);
+}
+
+void lanczos_interpolate_row(byte_t in[IN_WIDTH], byte_t out[OUT_WIDTH]) {
+    for (int xx = 0; xx < OUT_WIDTH; xx++) {
+        if (xx % SCALE == 0) {
+            out[xx] = in[xx / SCALE];
+            continue;
+        }
+        double x = (double) xx / SCALE;
+        double sum = 0;
+        for (int i = MAX(0, floor(x) - LANCZOS_A + 1); i <= MIN(IN_WIDTH - 1, floor(x) + LANCZOS_A); i++) {
+            sum += in[i] * lanczos_kernel(x - i);
+        }
+        out[xx] = (int)sum;
+    }
+}
+
+void lanczos_interpolate_col(byte_t img[OUT_HEIGHT][OUT_WIDTH], int col) {
+    // start filling from largest height first so we don't overwrite any pixels we're using
+    for (int xx = OUT_HEIGHT - 1; xx >= 0; xx--) {
+        if (xx % SCALE == 0) {
+            img[xx][col] = img[xx / SCALE][col];
+            continue;
+        }
+        double x = (double) xx / SCALE;
+        double sum = 0;
+        for (int i = MAX(0, floor(x) - LANCZOS_A + 1); i <= MIN(IN_HEIGHT - 1, floor(x) + LANCZOS_A); i++) {
+            sum += img[i][col] * lanczos_kernel(x - i);
+        }
+        img[xx][col] = (int)sum;
+    }
+}
 
 void lanczos(
     byte_t red_in[IN_HEIGHT][IN_WIDTH],
     byte_t blue_in[IN_HEIGHT][IN_WIDTH],
-    byte_t grn_in[IN_HEIGHT][IN_WIDTH],
+    byte_t green_in[IN_HEIGHT][IN_WIDTH],
     byte_t red_out[OUT_HEIGHT][OUT_WIDTH],
     byte_t blue_out[OUT_HEIGHT][OUT_WIDTH],
-    byte_t grn_out[OUT_HEIGHT][OUT_WIDTH]
+    byte_t green_out[OUT_HEIGHT][OUT_WIDTH]
 ) {
-    // TODO: implement
+    for (int i = 0; i < IN_HEIGHT; i++) {
+        lanczos_interpolate_row(red_in[i], red_out[i]);
+        lanczos_interpolate_row(blue_in[i], blue_out[i]);
+        lanczos_interpolate_row(green_in[i], green_out[i]);
+    }
+
+    for (int i = 0; i < OUT_WIDTH; i++) {
+        lanczos_interpolate_col(red_out, i);
+        lanczos_interpolate_col(blue_out, i);
+        lanczos_interpolate_col(green_out, i);
+    }
+
     return;
 }
--- LanczosUpscaler/lanczos.h
+++ LanczosUpscaler/lanczos.h
@@ -16,16 +16,29 @@
 #define OUT_WIDTH  3840
 #define OUT_HEIGHT 2160
 
+#define SCALE (OUT_WIDTH/IN_WIDTH)
+
+#define LANCZOS_A 2
+
 typedef uint8_t byte_t;
 
 // HLS target function
+void lanczos_HLS(
+    byte_t red_in[IN_HEIGHT][IN_WIDTH],
+    byte_t blue_in[IN_HEIGHT][IN_WIDTH],
+    byte_t green_in[IN_HEIGHT][IN_WIDTH],
+    byte_t red_out[OUT_HEIGHT][OUT_WIDTH],
+    byte_t blue_out[OUT_HEIGHT][OUT_WIDTH],
+    byte_t green_out[OUT_HEIGHT][OUT_WIDTH]
+);
+
 void lanczos(
     byte_t red_in[IN_HEIGHT][IN_WIDTH],
     byte_t blue_in[IN_HEIGHT][IN_WIDTH],
-    byte_t grn_in[IN_HEIGHT][IN_WIDTH],
+    byte_t green_in[IN_HEIGHT][IN_WIDTH],
     byte_t red_out[OUT_HEIGHT][OUT_WIDTH],
     byte_t blue_out[OUT_HEIGHT][OUT_WIDTH],
-    byte_t grn_out[OUT_HEIGHT][OUT_WIDTH]
+    byte_t green_out[OUT_HEIGHT][OUT_WIDTH]
 );
 
 #endif /* lanczos_h */
