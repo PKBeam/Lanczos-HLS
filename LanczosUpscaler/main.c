6ae41e6f40db6f711f608302c5f74fdedb2011cf
Vincent Liu <pkchu99@gmail.com>
2022-06-18 13:45:46 +1000

    Initial Commit

---
+++ LanczosUpscaler/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LanczosUpscaler
+//
+//  Created by Vincent Liu on 18/6/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
