5eafbf31d546f7f01e08a566a3aac7156f8284da
Ezra <other.ezhui@gmail.com>
2022-06-26 02:06:11 +1000

    Removed synthesis issue by taking variables out of loop bounds

--- LanczosUpscaler/lanczos.cpp
+++ LanczosUpscaler/lanczos.cpp
@@ -10,12 +10,16 @@
 
 typedef ap_fixed<BIT_PRECISION+3,3> kernel_in_t;
 
+#define FORALL(a,n) for (a = 0; a < n; a++)
 #define PI ((kernel_in_t)M_PI)
+
 kernel_t lanczos_kernel(kernel_in_t x){
-	kernel_t out = x==0? (kernel_t) 1.0 : (kernel_t) (LANCZOS_A*hls::sinpi(x)*hls::sinpi(x / LANCZOS_A) / (PI*PI*x*x));
-	return out;
+	if (x==0){
+		return (kernel_t) 1.0;
+	} else {
+		return (kernel_t) (LANCZOS_A*hls::sinpi(x)*hls::sinpi(x / LANCZOS_A) / (PI*PI*x*x));
+	}
 }
-#undef PI
 
 byte_t clamp_to_byte(num_t x){
 	if (x[BIT_PRECISION+8]){
@@ -25,38 +29,37 @@ byte_t clamp_to_byte(num_t x){
 	}
 
 }
-
 // HLS-synthesisable variant
 void lanczos(
-    byte_t img_in[NUM_CHANNELS][IN_HEIGHT][IN_WIDTH],
-    byte_t img_out[NUM_CHANNELS][OUT_HEIGHT][OUT_WIDTH]
+    byte_t img_input[NUM_CHANNELS][IN_HEIGHT][IN_WIDTH],
+    byte_t img_output[NUM_CHANNELS][OUT_HEIGHT][OUT_WIDTH]
 ) {
 	num_t img_processed_rows[NUM_CHANNELS][IN_HEIGHT][OUT_WIDTH];
     for (int j = 0; j < NUM_CHANNELS; j++) {
     	for (int i = 0; i < IN_HEIGHT; i++) {
 			for (int xx = 0; xx < OUT_WIDTH; xx++) {
 				int floorx = (SCALE_D * xx) / SCALE_N;
-				int start_idx = MAX(0, floorx - LANCZOS_A + 1);
-				int end_idx = MIN(IN_WIDTH - 1, floorx + LANCZOS_A);
 				num_t sum = 0;
-				for (int k = start_idx; k <= end_idx; k++) {
-					sum += img_in[j][i][k] * lanczos_kernel((kernel_in_t)((num_t)(xx*SCALE_D-k*SCALE_N)/SCALE_N));
-				}
+				for (int k = 1 - LANCZOS_A; k <= LANCZOS_A; k++){
+					int kk = k + floorx;
+					if (kk >= 0 && kk < IN_WIDTH){
+						sum += img_input[j][i][kk] * lanczos_kernel((kernel_in_t)((num_t)(xx*SCALE_D-kk*SCALE_N)/SCALE_N));
+					}
+                }
 				img_processed_rows[j][i][xx] = sum;
 			}
 		}
 		for (int i = 0; i < OUT_WIDTH; i++) {
-            for (int xx = OUT_HEIGHT - 1; xx >= 0; xx--) {
+            for (int xx = 0; xx < OUT_HEIGHT; xx++) {
 				int floorx = (SCALE_D * xx) / SCALE_N;
-				int start_idx = MAX(0, floorx - LANCZOS_A + 1);
-				int end_idx = MIN(IN_HEIGHT - 1, floorx + LANCZOS_A);
             	num_t sum = 0;
-				for (int k = start_idx; k <= end_idx; k++)
-				{
-                    sum += img_processed_rows[j][k][i] * lanczos_kernel((kernel_in_t)((num_t)(xx*SCALE_D-k*SCALE_N)/SCALE_N));
+				for (int k = 1 - LANCZOS_A; k <= LANCZOS_A; k++){
+					int kk = k + floorx;
+					if (kk >= 0 && kk < IN_HEIGHT){
+						sum += img_processed_rows[j][kk][i] * lanczos_kernel((kernel_in_t)((num_t)(xx*SCALE_D-kk*SCALE_N)/SCALE_N));
+					}
                 }
-
-                img_out[j][xx][i] = clamp_to_byte(sum);
+				img_output[j][xx][i] = clamp_to_byte(sum);
             }
         }
     }
